815b10c3afbf9ad616bb696430afa289b6204a0d
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2023-10-30 15:40:57 -0500

    update

--- cupertino_store/lib/src/ui/widgets/product_list_tab.dart
+++ cupertino_store/lib/src/ui/widgets/product_list_tab.dart
@@ -1,7 +1,5 @@
-import 'package:cupertino_store/src/config/theme/theme_bloc.dart';
 import 'package:cupertino_store/src/models/app_state_model.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'product_row_item.dart';
@@ -14,17 +12,11 @@ class ProductListTab extends StatelessWidget {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
         final products = model.getProducts();
-        final themeBloc = ThemeBloc();
         return CustomScrollView(
           semanticChildCount: products.length,
           slivers: <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: const Text('Cupertino Store'),
-              trailing: CupertinoButton(
-                  child: const Icon(Icons.brightness_4),
-                  onPressed: () {
-                    themeBloc.toggleTheme();
-                  }),
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Cupertino Store'),
             ),
             SliverSafeArea(
                 top: false,
